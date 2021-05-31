3011bf8952b432a8a7d09a8a9a5eea6add2504a2
adumbidiot <nathaniel.daniel23@outlook.com>
2021-05-30 17:57:44 -0700

    Add temporary `create_interface` function`

--- src/objbase.rs
+++ src/objbase.rs
@@ -1,5 +1,10 @@
-use winapi::shared::winerror::S_OK;
+use winapi::shared::guiddef::CLSID;
+use winapi::shared::winerror::FAILED;
+use winapi::shared::winerror::HRESULT;
+use winapi::shared::wtypesbase::CLSCTX_INPROC_SERVER;
+use winapi::um::combaseapi::CoCreateInstance;
 use winapi::um::combaseapi::CoIncrementMTAUsage;
+use winapi::Interface;
 
 // TODO: Consider returning cookie
 /// Init a MTA COM runtime. Only needs to be called once per process.
@@ -8,12 +13,35 @@ use winapi::um::combaseapi::CoIncrementMTAUsage;
 /// Returns an error if an MTA COM Runtime could not be created.
 pub fn init_mta_com_runtime() -> std::io::Result<()> {
     let mut cookie = std::ptr::null_mut();
-    let ret = unsafe { CoIncrementMTAUsage(&mut cookie) };
+    let code = unsafe { CoIncrementMTAUsage(&mut cookie) };
 
-    match ret {
-        S_OK => Ok(()),
-        code => Err(std::io::Error::from_raw_os_error(code)),
+    if FAILED(code) {
+        return Err(std::io::Error::from_raw_os_error(code));
     }
+
+    Ok(())
+}
+
+// TODO: Try to make a safe but less flexible abstraction for this.
+/// Make a new com object from the given class ID.
+///
+/// # Safety
+/// The returned type must match the input class ID.
+pub unsafe fn create_instance<T: Interface>(class_id: &CLSID) -> Result<*mut T, HRESULT> {
+    let mut instance = std::ptr::null_mut();
+    let hr = CoCreateInstance(
+        class_id,
+        std::ptr::null_mut(),
+        CLSCTX_INPROC_SERVER,
+        &T::uuidof(),
+        &mut instance,
+    );
+
+    if FAILED(hr) {
+        return Err(hr);
+    }
+
+    Ok(instance.cast())
 }
 
 #[cfg(test)]
